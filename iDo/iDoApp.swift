359d376b1fd7b03d7a4566abe9c203c7a2d1cc9e
Aleksandar Drakaliyski <theall3xander@gmail.com>
2022-11-07 14:53:19 +0000

    Initial Commit

---
+++ iDo/iDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDoApp.swift
+//  iDo
+//
+//  Created by Aleksandar Drakaliyski on 07/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct iDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
